8af3c1892aa75744e5dc7d79918c874af0aae244
dracu-lah <nevilnicks4321@gmail.com>
2025-01-27 03:43:04 +0530

    added input values

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,9 +1,37 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
 
-export const Route = createLazyFileRoute('/about')({
+export const Route = createLazyFileRoute("/about")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+  const [value, setValue] = useState({ name: "", about: "", style: "" });
+  console.log("value", value);
+  return (
+    <div>
+      Hello "/about"!
+      <div>
+        <input
+          type="text"
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <input
+          type="text"
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, about: e.target.value }))
+          }
+        />
+
+        <input
+          type="text"
+          onChange={(e) =>
+            setValue((prev) => ({ ...prev, style: e.target.value }))
+          }
+        />
+      </div>
+    </div>
+  );
 }
