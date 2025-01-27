9d82d57e9c92fe37f330249acb3b45657500485c
dracu-lah <nevilnicks4321@gmail.com>
2025-01-27 14:10:07 +0530

    moved auth file

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -1,10 +1,10 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/_authenticated")({
+export const Route = createFileRoute('/_authenticated')({
   beforeLoad: async ({ context }) => {
-    const { isLogged } = context.authentication;
+    const { isLogged } = context.authentication
     if (!isLogged()) {
-      throw redirect({ to: "/auth/login" });
+      throw redirect({ to: '/auth/login' })
     }
   },
-});
+})
