e436b4cb403dd28be31661b3d70844d3315e21ba
dracu-lah <nevilnicks4321@gmail.com>
2025-01-27 14:40:47 +0530

    added login text

--- src/components/common/layout/Header/index.tsx
+++ src/components/common/layout/Header/index.tsx
@@ -1,8 +1,12 @@
+import { useAuth } from "@/hooks/useAuth";
+
 const headerStyles = {
   header: {
     padding: "1rem",
     backgroundColor: "#f8f9fa",
     borderBottom: "1px solid #dee2e6",
+    display: "flex",
+    justifyContent: "space-between",
   },
   title: {
     margin: 0,
@@ -10,9 +14,18 @@ const headerStyles = {
     color: "#212529",
   },
 };
-
-export const Header = () => (
-  <header style={headerStyles.header}>
-    <h1 style={headerStyles.title}>My App</h1>
-  </header>
-);
+export const Header = () => {
+  const { isLogged, signOut } = useAuth();
+  return (
+    <header style={headerStyles.header}>
+      {" "}
+      <h1 style={headerStyles.title}>My App</h1>
+      {isLogged() && (
+        <div>
+          <p>Logged In Vro</p>
+          <button onClick={() => signOut()}>Log out</button>
+        </div>
+      )}
+    </header>
+  );
+};
--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useAuth } from "@/hooks/useAuth";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/auth/login')({
+export const Route = createFileRoute("/auth/login")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/auth/login"!</div>
+  const { signIn } = useAuth();
+  return (
+    <div>
+      Hello "/auth/login"!
+      <br />
+      <button onClick={() => signIn()}>Login Cheyada</button>
+    </div>
+  );
 }
